222ac9ef43a4429b232fd5fbb1cf2e3029e540dd
doritoes <39832079+doritoes@users.noreply.github.com>
2023-02-20 20:18:32 -0500

    Update create_CPBASE.bat

--- Chapter04/Scripts/create_CPBASE.bat
+++ Chapter04/Scripts/create_CPBASE.bat
@@ -8,7 +8,7 @@ VBoxManage storageattach %VM% --storagectl "IDE" --port 0 --device 0 --type dvdd
 VBoxManage storagectl %VM% --name "SATA" --add SAS --controller LsiLogicSas
 VBoxManage storageattach %VM% --storagectl "SATA" --port 0 --device 0 --type hdd --medium %VM%.vdi
 VBoxManage modifyvm %VM% --usb off
-VBoxManage modifyvm %VM% --audio none
+VBoxManage modifyvm %VM% --audio-driver none
 VBoxManage modifyvm %VM% --cpus 4
 VBoxManage modifyvm %VM% --ioapic on
 VBoxManage modifyvm %VM% --paravirtprovider none
